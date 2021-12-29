33f89a9db0ee41d71da0659ac9287fd1380793b6
Mathias Michael <leesanmoj@gmail.com>
2021-12-29 22:17:50 +0100

    doc: Improved documentation on "etl.py" file

--- etl.py
+++ etl.py
@@ -4,21 +4,45 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    '''
+    Stage data from S3 to Redshift staging tables (staging_events, staging_song). 
+
+    Parameters
+    ----
+    cur: psycopg2.extensions.cursor
+        Allows python code to execute PostgreSQL command in Redshift.
+    conn: psycopg2.extensions.connection
+        Connection to a PostgreSQL database instance (In this case, Redshift)
+    '''
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    '''
+    Performs SQL to SQL ETL from staging tables to Star Dimension tables
+    for easy analysis. 
+
+    Parameters
+    ----
+    cur: psycopg2.extensions.cursor
+        Allows python code to execute PostgreSQL command in Redshift.
+    conn: psycopg2.extensions.connection
+        Connection to a PostgreSQL database instance (In this case, Redshift)
+    '''
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    # Loading configuration from 'dwh.cfg' file.
+    # Open 'configuration.ipynb' to set up configuration
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
+    # Connects to PostgreSQL database instance(Redshift) and get cursor.
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
     
