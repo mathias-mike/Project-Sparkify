b3ddc3c59712edff9b5e01eeabfa2ecd43af7e47
Mathias Michael <leesanmoj@gmail.com>
2021-12-29 22:04:41 +0100

    feat: Create "spark_stagging.py" file Loading data directly from the s3 path provided was taking too long because of the way the file was saved (nexted directory).
    In spark_stagging.py, I plan to fire up a spark cluster and use it
    to load the data from s3 to Redshift.

--- create_tables.py
+++ create_tables.py
@@ -4,21 +4,47 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    '''
+    Drops all existing tables in the Redshift cluster. 
+    Runs "sql_queries.drop_table_queries" : A list of PostgreSQL DROP TABLE queries
+
+    Parameters
+    ----
+    cur: psycopg2.extensions.cursor
+        Allows python code to execute PostgreSQL command in Redshift.
+    conn: psycopg2.extensions.connection
+        Connection to a PostgreSQL database instance (In this case, Redshift)
+    '''
+
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    '''
+    Create all tables in the Redshift cluster. 
+    Runs "sql_queries.create_table_queries" : A list of PostgreSQL CREATE TABLE queries
+
+    Parameters
+    ----
+    cur: psycopg2.extensions.cursor
+        Allows python code to execute PostgreSQL command in Redshift.
+    conn: psycopg2.extensions.connection
+        Connection to a PostgreSQL database instance (In this case, Redshift)
+    '''
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    # Loading configuration from 'dwh.cfg' file.
+    # Open 'configuration.ipynb' to set up configuration
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
+    # Connects to PostgreSQL database instance(Redshift) and get cursor.
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
 
---
+++ spark_stagging.py
@@ -0,0 +1,79 @@
+from pyspark.sql import SparkSession
+
+# The dataset takes a long will to load with Postgresql COPY command.
+# This might possibly be caused by the way the file is save (nexted folders).
+
+# In a bid to improve on the perfomance, I fired up a Spark Cluster and 
+# ran this script to load the data and save it in a compressed "csv.gzip"
+# format on s3. That way I would be easy to read into Redshift. 
+
+# Better would be to directly stage the file from spark to Redshift....
+# Working on it
+
+
+def read_from_s3(spark, path):
+    '''
+    Reads data from s3 path
+
+    Parameters
+    ----
+    spark: SparkSession
+        Entry point into all the functionality in Spark
+    path: str
+        S3 path to read data from
+
+    Return
+    ----
+    Spark DataFrame
+        Data loaded into column and row format
+    '''
+
+    return spark.read.json(path)
+
+
+def write_to_s3(dataframe, path):
+    '''
+    Write data to s3 path
+
+    Parameters
+    ----
+    dataframe: Spark DataFrame
+        Data to load into s3
+    path: str
+        S3 path to write data to
+    '''
+    
+    dataframe.write \
+        .save(path, format='csv', compression='gzip', header=True)
+
+
+def main():
+    # Instantiating spark object
+    spark = SparkSession \
+            .builder \
+            .config("spark.sql.catalogImplementation","in-memory") \
+            .getOrCreate()
+
+
+    # Loading log-data
+    log_read_path = "s3://udacity-dend/log-data/*/*/*.json"
+    log_data = read_from_s3(spark, log_read_path)
+
+    # Loading song-data
+    song_read_path = "s3://udacity-dend/song-data/*/*/*/*.json"
+    song_data = read_from_s3(spark, song_read_path)
+    # song_data = spark.read.option("Header", True) \
+    #             .csv('s3://sparkify-dwh-bucket/song_data.csv/*.csv')
+
+
+    # Writing log data in compressed form to s3 path
+    log_write_path = 's3://sparkify-dwh-bucket/log-data'
+    write_to_s3(log_data, log_write_path)
+
+    # Writing song data in compressed form to s3 path
+    song_write_path = 's3://sparkify-dwh-bucket/song-data'
+    write_to_s3(song_data, song_write_path)
+
+if __name__ == "__main__":
+    main()
+
