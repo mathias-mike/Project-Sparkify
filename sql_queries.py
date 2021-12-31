13a1905456494ad0f2e0a6f4be7b26111adef4fb
Mathias Michael <leesanmoj@gmail.com>
2021-12-31 13:41:14 +0100

    fix: Added data quality constraints on sqlqueries.py

--- sql_queries.py
+++ sql_queries.py
@@ -31,16 +31,16 @@ staging_events_table_create= ("""
         "gender" VARCHAR,
         "itemInSession" SMALLINT,
         "lastName" VARCHAR,
-        "length" REAL,
+        "length" DECIMAL(24, 8),
         "level" VARCHAR,
         "location" VARCHAR,
         "method" VARCHAR,
         "page" VARCHAR,
-        "registration" DOUBLE PRECISION,
+        "registration" VARCHAR,
         "sessionId" SMALLINT,
         "song" VARCHAR,
         "status" SMALLINT,
-        "ts" BIGINT,
+        "ts" TIMESTAMP,
         "userAgent" VARCHAR,
         "userId" SMALLINT
     )
@@ -53,7 +53,7 @@ staging_songs_table_create = ("""
         "artist_location" VARCHAR(MAX),
         "artist_longitude" DECIMAL(18,12),
         "artist_name" VARCHAR(MAX),
-        "duration" DOUBLE PRECISION, 
+        "duration" DECIMAL(24, 8), 
         "num_songs" SMALLINT,
         "song_id" VARCHAR,
         "title" VARCHAR(MAX),
@@ -65,21 +65,22 @@ staging_songs_table_create = ("""
 songplay_table_create = ("""
     CREATE TABLE IF NOT EXISTS "songplays" (
         "songplay_id" INT IDENTITY(0, 1),
-        "start_time" BIGINT NOT NULL SORTKEY,
-        "user_id" SMALLINT NOT NULL,
+        "start_time" TIMESTAMP NOT NULL SORTKEY REFERENCES "time" ("start_time"),
+        "user_id" SMALLINT NOT NULL REFERENCES "users" ("user_id"),
         "level" VARCHAR(5) NOT NULL,
-        "song_id" VARCHAR(20) NOT NULL DISTKEY,
-        "artist_id" VARCHAR(20) NOT NULL,
+        "song_id" VARCHAR(20) NOT NULL DISTKEY REFERENCES "songs" ("song_id"),
+        "artist_id" VARCHAR(20) NOT NULL REFERENCES "artists" ("artist_id"),
         "session_id" SMALLINT NOT NULL,
         "location" VARCHAR NOT NULL,
-        "user_agent" VARCHAR NOT NULL
+        "user_agent" VARCHAR NOT NULL,
+        PRIMARY KEY ("songplay_id") 
     )
 """)
 
 # DIMENSION TABLES
 user_table_create = ("""
     CREATE TABLE IF NOT EXISTS "users" (
-        "user_id" SMALLINT NOT NULL SORTKEY,
+        "user_id" SMALLINT NOT NULL SORTKEY PRIMARY KEY,
         "first_name" VARCHAR NOT NULL,
         "last_name" VARCHAR NOT NULL,
         "gender" VARCHAR(2) NOT NULL,
@@ -90,7 +91,7 @@ user_table_create = ("""
 
 song_table_create = ("""
     CREATE TABLE IF NOT EXISTS "songs" (
-        "song_id" VARCHAR(20) NOT NULL SORTKEY DISTKEY,
+        "song_id" VARCHAR(20) NOT NULL SORTKEY DISTKEY PRIMARY KEY,
         "title" VARCHAR(MAX) NOT NULL,
         "artist_id" VARCHAR(20) NOT NULL,
         "year" SMALLINT NOT NULL,
@@ -100,7 +101,7 @@ song_table_create = ("""
 
 artist_table_create = ("""
     CREATE TABLE IF NOT EXISTS "artists" (
-        "artist_id" VARCHAR(20) NOT NULL SORTKEY,
+        "artist_id" VARCHAR(20) NOT NULL SORTKEY PRIMARY KEY,
         "name" VARCHAR(MAX) NOT NULL,
         "location" VARCHAR(MAX),
         "latitude" DECIMAL(18, 12),
@@ -111,7 +112,7 @@ artist_table_create = ("""
 
 time_table_create = ("""
     CREATE TABLE IF NOT EXISTS "time" (
-        start_time BIGINT NOT NULL SORTKEY,
+        start_time TIMESTAMP NOT NULL SORTKEY PRIMARY KEY,
         hour SMALLINT NOT NULL,
         day SMALLINT NOT NULL,
         week SMALLINT NOT NULL,
@@ -144,7 +145,7 @@ staging_songs_copy = ("""
 #     COPY "staging_events" FROM 's3://sparkify-dwh-bucket/log-data-json'
 #     CREDENTIALS 'aws_iam_role={}'
 #     COMPUPDATE OFF REGION 'us-west-2'
-#     JSON 'auto ignorecase'
+#     JSON 'auto ignorecase' ACCEPTINVCHARS TIMEFORMAT AS 'epochmillisecs'
 # """).format(arn)
 
 # staging_songs_copy = ("""
@@ -164,7 +165,9 @@ songplay_table_insert = ("""
         s.artist_id AS artist_id, e.sessionid AS session_id, e.location AS location, e.useragent AS user_agent
     FROM staging_events e
     JOIN staging_songs s
-    ON e.song = s.title AND e.artist = s.artist_name
+    ON e.song = s.title 
+    AND e.artist = s.artist_name
+    AND e.length = s.duration
     WHERE e.page = 'NextSong'
 """)
 
@@ -202,19 +205,38 @@ artist_table_insert = ("""
 time_table_insert = ("""
     INSERT INTO "time" ("start_time", "hour", "day", "week", "month", "year", "weekday")
     SELECT DISTINCT ts AS start_time,
-            DATE_PART('hour', TIMESTAMP 'epoch' + (ts/1000) * INTERVAL '1 second') AS hour,
-            DATE_PART('day', TIMESTAMP 'epoch' + (ts/1000) * INTERVAL '1 second') AS day,
-            DATE_PART('week', TIMESTAMP 'epoch' + (ts/1000) * INTERVAL '1 second') AS week,
-            DATE_PART('month', TIMESTAMP 'epoch' + (ts/1000) * INTERVAL '1 second') AS month,
-            DATE_PART('year', TIMESTAMP 'epoch' + (ts/1000) * INTERVAL '1 second') AS year,
-            DATE_PART('weekday', TIMESTAMP 'epoch' + (ts/1000) * INTERVAL '1 second') AS weekday
+            DATE_PART('hour', ts) AS hour,
+            DATE_PART('day', ts) AS day,
+            DATE_PART('week', ts) AS week,
+            DATE_PART('month', ts) AS month,
+            DATE_PART('year', ts) AS year,
+            DATE_PART('weekday', ts) AS weekday
     FROM staging_events
 """)
 
 
+# time_table_insert = ("""
+#     INSERT INTO "time" ("start_time", "hour", "day", "week", "month", "year", "weekday")
+#     SELECT DISTINCT ts AS start_time,
+#             DATE_PART('hour', TIMESTAMP 'epoch' + (ts/1000) * INTERVAL '1 second') AS hour,
+#             DATE_PART('day', TIMESTAMP 'epoch' + (ts/1000) * INTERVAL '1 second') AS day,
+#             DATE_PART('week', TIMESTAMP 'epoch' + (ts/1000) * INTERVAL '1 second') AS week,
+#             DATE_PART('month', TIMESTAMP 'epoch' + (ts/1000) * INTERVAL '1 second') AS month,
+#             DATE_PART('year', TIMESTAMP 'epoch' + (ts/1000) * INTERVAL '1 second') AS year,
+#             DATE_PART('weekday', TIMESTAMP 'epoch' + (ts/1000) * INTERVAL '1 second') AS weekday
+#     FROM staging_events
+# """)
+
+
 
 # QUERY LISTS
-create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+# Please note that creation order matters. Tables with primary keys must be created
+# before the table that reference them via foriegn keys
+create_table_queries = [staging_events_table_create, staging_songs_table_create, user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
+
+# Please note that drop order matters. Table with reference to other tables must be dropped 
+# before the tables they reference can be dropped.
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
